c1d50fc3a8d0fd91c750594146fbb667e3534adc
Eugene Lebedev <duke.tougu@gmail.com>
2019-07-05 16:48:47 +0300

    [+] project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.41"
+    id("io.gitlab.arturbosch.detekt") version "1.0.0-RC15"
+}
+
+group = "ru.lebe.dev.mrjanitor"
+version = "1.0.0-BETA"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("org.slf4j:slf4j-api:1.7.25")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.2.0")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.2.0")
+
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform { }
+}
+
+detekt {
+    toolVersion = "1.0.0-RC15"
+
+    input = project.files("src/main/kotlin")
+
+    reports {
+        html {
+            enabled = true
+            destination = File("detekt.html")
+        }
+    }
+}
---
+++ src/main/kotlin/ru/lebe/dev/mrjanitor/App.kt
@@ -0,0 +1,12 @@
+package ru.lebe.dev.mrjanitor
+
+class App {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Mr.Janitor")
+        }
+    }
+
+}
