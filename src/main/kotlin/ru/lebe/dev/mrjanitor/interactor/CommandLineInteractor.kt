e116d1ccf2229135653ccb9cf1d961372c0c10c6
Eugene Lebedev <duke.tougu@gmail.com>
2019-07-18 15:47:56 +0300

    [~] refactoring

--- src/main/kotlin/ru/lebe/dev/mrjanitor/interactor/CommandLineInteractor.kt
+++ src/main/kotlin/ru/lebe/dev/mrjanitor/interactor/CommandLineInteractor.kt
@@ -1,12 +1,14 @@
 package ru.lebe.dev.mrjanitor.interactor
 
 import arrow.core.Either
+import ru.lebe.dev.mrjanitor.domain.DirectoryItem
+import ru.lebe.dev.mrjanitor.domain.FileItem
 import ru.lebe.dev.mrjanitor.domain.Profile
 import ru.lebe.dev.mrjanitor.domain.StorageUnit
 import ru.lebe.dev.mrjanitor.presenter.AppPresenter
 import ru.lebe.dev.mrjanitor.usecase.GetDirectoryItemsForCleanUp
 import ru.lebe.dev.mrjanitor.usecase.GetFileItemsForCleanUp
-import java.util.*
+import java.util.Date
 
 class CommandLineInteractor(
     private val getFileItemsForCleanUp: GetFileItemsForCleanUp,
@@ -20,29 +22,7 @@ class CommandLineInteractor(
             when(profile.storageUnit) {
                 StorageUnit.DIRECTORY -> {
                     when(val directoryItems = getDirectoryItemsForCleanUp.getItems(profile)) {
-                        is Either.Right -> {
-
-                            if (directoryItems.b.isNotEmpty()) {
-
-                                presenter.showMessage("directory items for clean up:")
-
-                                directoryItems.b.forEach { directoryItem ->
-                                    presenter.showMessage("- '${directoryItem.name}'")
-                                    presenter.showMessage("  - valid: ${directoryItem.valid}")
-                                    presenter.showMessage(
-                                        "  - last-modified: " +
-                                                "${Date(directoryItem.path.toFile().lastModified())}"
-                                    )
-                                }
-
-                                presenter.showMessage("---")
-                                presenter.showMessage("items total: ${directoryItems.b.size}")
-
-                            } else {
-                                presenter.showMessage("nothing to clean up ;)")
-                            }
-
-                        }
+                        is Either.Right -> showDirectoryItemsForCleanUp(directoryItems.b)
                         is Either.Left -> {
                             presenter.showError("unable to get directory items " +
                                     "for cleanup, profile '${profile.name}'")
@@ -52,26 +32,7 @@ class CommandLineInteractor(
                 StorageUnit.FILE -> {
 
                     when(val fileItems = getFileItemsForCleanUp.getFileItems(profile)) {
-                        is Either.Right -> {
-
-                            if (fileItems.b.isNotEmpty()) {
-
-                                fileItems.b.forEach { fileItem ->
-                                    presenter.showMessage("- '${fileItem.name}'")
-                                    presenter.showMessage("  - valid: ${fileItem.valid}")
-                                    presenter.showMessage(
-                                        "  - last-modified: ${Date(fileItem.path.toFile().lastModified())}"
-                                    )
-                                }
-
-                                presenter.showMessage("---")
-                                presenter.showMessage("items total: ${fileItems.b.size}")
-
-                            } else {
-                                presenter.showMessage("nothing to clean up ;)")
-                            }
-
-                        }
+                        is Either.Right -> showFileItemsForCleanUp(fileItems.b)
                         is Either.Left -> {
                             presenter.showError("unable to get file items " +
                                     "for cleanup, profile '${profile.name}'")
@@ -80,6 +41,47 @@ class CommandLineInteractor(
 
                 }
             }
+        }
+    }
+
+    private fun showDirectoryItemsForCleanUp(directoryItems: List<DirectoryItem>) {
+        if (directoryItems.isNotEmpty()) {
+
+            presenter.showMessage("directory items for clean up:")
+
+            directoryItems.forEach { directoryItem ->
+                presenter.showMessage("- '${directoryItem.name}'")
+                presenter.showMessage("  - valid: ${directoryItem.valid}")
+                presenter.showMessage(
+                    "  - last-modified: " +
+                            "${Date(directoryItem.path.toFile().lastModified())}"
+                )
+            }
+
+            presenter.showMessage("---")
+            presenter.showMessage("items total: ${directoryItems.size}")
+
+        } else {
+            presenter.showMessage("nothing to clean up ;)")
+        }
+    }
+
+    private fun showFileItemsForCleanUp(fileItems: List<FileItem>) {
+        if (fileItems.isNotEmpty()) {
+
+            fileItems.forEach { fileItem ->
+                presenter.showMessage("- '${fileItem.name}'")
+                presenter.showMessage("  - valid: ${fileItem.valid}")
+                presenter.showMessage(
+                    "  - last-modified: ${Date(fileItem.path.toFile().lastModified())}"
+                )
+            }
+
+            presenter.showMessage("---")
+            presenter.showMessage("items total: ${fileItems.size}")
+
+        } else {
+            presenter.showMessage("nothing to clean up ;)")
         }
     }
 }
