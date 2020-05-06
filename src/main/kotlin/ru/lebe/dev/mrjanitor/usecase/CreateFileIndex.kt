6fe120f0030b3eb55b1a7dcd40b7bbab07147fb1
Eugene Lebedev <duke.tougu@gmail.com>
2020-05-06 14:24:06 +0300

    [~] use operation-result as return type

--- src/main/kotlin/ru/lebe/dev/mrjanitor/usecase/CreateFileIndex.kt
+++ src/main/kotlin/ru/lebe/dev/mrjanitor/usecase/CreateFileIndex.kt
@@ -1,26 +1,23 @@
 package ru.lebe.dev.mrjanitor.usecase
 
 import arrow.core.Either
-import arrow.core.Failure
-import arrow.core.Success
-import arrow.core.Try
 import org.apache.commons.codec.digest.DigestUtils
 import org.slf4j.LoggerFactory
 import ru.lebe.dev.mrjanitor.domain.DirectoryItem
 import ru.lebe.dev.mrjanitor.domain.FileItem
 import ru.lebe.dev.mrjanitor.domain.OperationError
+import ru.lebe.dev.mrjanitor.domain.OperationResult
 import ru.lebe.dev.mrjanitor.domain.PathFileIndex
 import ru.lebe.dev.mrjanitor.domain.Profile
 import ru.lebe.dev.mrjanitor.domain.StorageUnit
 import java.io.File
-import java.io.IOException
 import java.nio.file.Path
 import java.nio.file.Paths
 
 class CreateFileIndex {
     private val log = LoggerFactory.getLogger(javaClass)
 
-    fun create(profile: Profile): Either<OperationError, PathFileIndex> {
+    fun create(profile: Profile): OperationResult<PathFileIndex> {
         log.info("create file index for path '${profile.path}'")
         log.info("- storage-unit: ${profile.storageUnit}")
 
@@ -44,125 +41,141 @@ class CreateFileIndex {
     }
 
     private fun createIndexForDirectories(path: Path, directoryNameFilter: Regex,
-                              fileNameFilter: Regex, md5HashRequired: Boolean): Either<OperationError, PathFileIndex> =
+                              fileNameFilter: Regex, md5HashRequired: Boolean): OperationResult<PathFileIndex> =
 
         when(val directoryItems = getDirectoryItemsFromPath(
                 path, directoryNameFilter, fileNameFilter, md5HashRequired)
             ) {
-            is Success -> {
+            is Either.Right -> {
                 log.info("index has been created")
-                log.debug(directoryItems.value.toString())
+                log.debug(directoryItems.b.toString())
 
                 Either.right(
                     PathFileIndex(
                         path = path,
                         storageUnit = StorageUnit.DIRECTORY,
-                        directoryItems = directoryItems.value,
+                        directoryItems = directoryItems.b,
                         fileItems = listOf()
                     )
                 )
             }
-            is Failure -> {
-                log.error("unable to create file index")
-                log.error(directoryItems.exception.message, directoryItems.exception.cause)
+            is Either.Left -> {
+                log.error("unable to create file index for path '$path'")
                 Either.left(OperationError.ERROR)
             }
         }
 
     private fun createIndexForFiles(path: Path, fileNameFilter: Regex,
-                                    md5HashRequired: Boolean): Either<OperationError, PathFileIndex> =
+                                    md5HashRequired: Boolean): OperationResult<PathFileIndex> =
         when(val fileItems = getFileItemsFromPath(path, fileNameFilter, md5HashRequired)) {
-            is Try.Success -> {
+            is Either.Right -> {
                 Either.right(
                     PathFileIndex(
                         path = path,
                         storageUnit = StorageUnit.FILE,
                         directoryItems = listOf(),
-                        fileItems = fileItems.value
+                        fileItems = fileItems.b
                     )
                 )
             }
-            is Try.Failure -> {
-                log.error(
-                    "unable to get file items from path: ${fileItems.exception.message}",
-                    fileItems.exception.cause
-                )
+            is Either.Left -> {
+                log.error("unable to get file items from path: $path")
                 Either.left(OperationError.ERROR)
             }
         }
 
     private fun getDirectoryItemsFromPath(path: Path, directoryNameFilter: Regex,
-                                          fileNameFilter: Regex, md5HashRequired: Boolean) = Try<List<DirectoryItem>> {
+                                          fileNameFilter: Regex,
+                                          md5HashRequired: Boolean): OperationResult<List<DirectoryItem>> =
 
-        log.debug("get directory items from path '$path', directory-name-filter '$directoryNameFilter'")
-        log.debug("file-name-filter '$fileNameFilter', md5-hash-required: $md5HashRequired")
+        try {
+            log.debug("get directory items from path '$path', directory-name-filter '$directoryNameFilter'")
+            log.debug("file-name-filter '$fileNameFilter', md5-hash-required: $md5HashRequired")
 
-        val results = arrayListOf<DirectoryItem>()
+            var hasErrors = false
 
-        path.toFile().listFiles()?.filter { it.isDirectory && directoryNameFilter.matches(it.name) }
-                                 ?.sortedBy { it.name }?.forEach { directory ->
+            val results = arrayListOf<DirectoryItem>()
 
-            when(val fileItems = getFileItemsFromPath(directory.toPath(), fileNameFilter, md5HashRequired)) {
-                is Success -> {
-                    log.debug("file-items obtained: ${fileItems.value.size}")
+            path.toFile().listFiles()?.filter { it.isDirectory && directoryNameFilter.matches(it.name) }
+                                     ?.sortedBy { it.name }?.forEach { directory ->
 
-                    var directorySize = 0L
+                if (!hasErrors) {
+                    when(val fileItems =
+                                        getFileItemsFromPath(directory.toPath(), fileNameFilter, md5HashRequired)) {
+                        is Either.Right -> {
+                            log.debug("file-items obtained: ${fileItems.b.size}")
 
-                    fileItems.value.forEach { directorySize += it.size }
+                            var directorySize = 0L
 
-                    log.debug("- directory size: $directorySize")
+                            fileItems.b.forEach { directorySize += it.size }
 
-                    val directoryItem = DirectoryItem(
-                        path = directory.toPath(),
-                        name = directory.name,
-                        size = directorySize,
-                        fileItems = fileItems.value,
+                            log.debug("- directory size: $directorySize")
+
+                            val directoryItem = DirectoryItem(
+                                path = directory.toPath(),
+                                name = directory.name,
+                                size = directorySize,
+                                fileItems = fileItems.b,
+                                valid = false
+                            )
+
+                            log.debug("- file items: ${fileItems.b.size}")
+
+                            results += directoryItem
+                        }
+                        is Either.Left -> hasErrors = true
+                    }
+                }
+            }
+
+            log.debug("directory items obtained: ${results.size}")
+
+            if (!hasErrors) {
+                Either.right(results)
+
+            } else {
+                Either.left(OperationError.ERROR)
+            }
+
+        } catch (e: Exception) {
+            log.error("unable to get directory items from path '$path': ${e.message}", e)
+            Either.left(OperationError.ERROR)
+        }
+
+    private fun getFileItemsFromPath(path: Path, fileNameFilter: Regex,
+                                     md5HashRequired: Boolean): OperationResult<List<FileItem>> =
+        try {
+            log.debug("get file items from path '$path', file-name-filter '$fileNameFilter', " +
+                "md5-hash-required: $md5HashRequired")
+
+            val results = arrayListOf<FileItem>()
+
+            path.toFile().listFiles()?.filter { it.isFile && fileNameFilter.matches(it.name) }
+                ?.filterNot { it.extension.toLowerCase() in listOf("md5", "log") }
+                ?.sortedBy { it.name }
+                ?.forEach { file ->
+
+                    val fileItem = FileItem(
+                        path = file.absoluteFile.toPath(),
+                        name = file.name,
+                        size = file.length(),
+                        hash = getMd5Hash(file, md5HashRequired),
                         valid = false
                     )
 
-                    log.debug("- file items: ${fileItems.value.size}")
+                    log.debug(fileItem.toString())
 
-                    results += directoryItem
+                    results += fileItem
                 }
-                is Failure -> throw IOException("unable to get file items for path: ${fileItems.exception.message}")
-            }
 
+            log.debug("- items: ${results.size}")
+
+            Either.right(results)
+
+        } catch (e: Exception) {
+            log.error("unable to get files items from path '$path': ${e.message}", e)
+            Either.left(OperationError.ERROR)
         }
-
-        log.debug("directory items obtained: ${results.size}")
-
-        results
-    }
-
-    private fun getFileItemsFromPath(path: Path, fileNameFilter: Regex,
-                                     md5HashRequired: Boolean) = Try<List<FileItem>> {
-        log.debug("get file items from path '$path', file-name-filter '$fileNameFilter', " +
-                  "md5-hash-required: $md5HashRequired")
-
-        val results = arrayListOf<FileItem>()
-
-        path.toFile().listFiles()?.filter { it.isFile && fileNameFilter.matches(it.name) }
-                                 ?.filterNot { it.extension.toLowerCase() in listOf("md5", "log") }
-                                 ?.sortedBy { it.name }
-                                 ?.forEach { file ->
-
-            val fileItem = FileItem(
-                path = file.absoluteFile.toPath(),
-                name = file.name,
-                size = file.length(),
-                hash = getMd5Hash(file, md5HashRequired),
-                valid = false
-            )
-
-            log.debug(fileItem.toString())
-
-            results += fileItem
-        }
-
-        log.debug("- items: ${results.size}")
-
-        results
-    }
 
     private fun getMd5Hash(file: File, md5HashRequired: Boolean) =
             if (md5HashRequired) {
